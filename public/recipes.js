251a1f4eb106a36a9f7d7149bdcb5182f42dde88
jvmMachado <joaovmmachado@gmail.com>
2020-03-19 09:27:45 -0300

    Add dynamic recipe page

--- public/recipes.js
+++ public/recipes.js
@@ -1,33 +1,41 @@
 const cards = document.querySelectorAll('.card');
-const modal = document.querySelector('.modal-overlay');
-const closeButton = modal.querySelector('i');
+// const modal = document.querySelector('.modal-overlay');
+// const closeButton = modal.querySelector('i');
 
-function showModal(modalImage, modalTitle, modalAuthor) {
-  modal.classList.add('modal-active');
+// function showModal(modalImage, modalTitle, modalAuthor) {
+//   modal.classList.add('modal-active');
 
-  modal.querySelector('img').setAttribute('src', modalImage);
-  modal.querySelector('h2').innerHTML = modalTitle;
-  modal.querySelector('p').innerHTML = modalAuthor;
-}
+//   modal.querySelector('img').setAttribute('src', modalImage);
+//   modal.querySelector('h2').innerHTML = modalTitle;
+//   modal.querySelector('p').innerHTML = modalAuthor;
+// }
 
-function hideModal() {
-  modal.classList.remove('modal-active');
-}
+// function hideModal() {
+//   modal.classList.remove('modal-active');
+// }
 
 for (let card of cards) {
-  const modalImage = card.querySelector('img').getAttribute('src');
-  const modalTitle = card.querySelector('h3').innerHTML;
-  const modalAuthor = card.querySelector('p').innerHTML;
-  console.log(modalImage, modalTitle, modalAuthor);
+  // const modalImage = card.querySelector('img').getAttribute('src');
+  // const modalTitle = card.querySelector('h3').innerHTML;
+  // const modalAuthor = card.querySelector('p').innerHTML;
+  const recipeId = card.getAttribute('id');
+  const cardImg = card.querySelector('img');
   card.addEventListener('click', () => {
-    showModal(modalImage, modalTitle, modalAuthor);
+    window.location = `/recipe/${recipeId}`;
   });
+  
+
+
+
+  // card.addEventListener('click', () => {
+  //   showModal(modalImage, modalTitle, modalAuthor);
+  // });
 }
 
-closeButton.addEventListener('click', hideModal);
+// closeButton.addEventListener('click', hideModal);
 
-document.addEventListener('click', e => {
-  if (e.target === modal) {
-    hideModal();
-  }
-});
+// document.addEventListener('click', e => {
+//   if (e.target === modal) {
+//     hideModal();
+//   }
+// });
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const recipes = require('./data');
 
 const server = express();
 nunjucks.configure('views', { express: server, autoescape: true, noCache: true });
@@ -8,7 +9,7 @@ server.use(express.static('public'));
 server.set('view engine', 'njk');
 
 server.get('/', (req, res) => {
-  return res.render('index');
+  return res.render('index', { recipes });
 })
 
 server.get('/about', (req, res) => {
@@ -16,7 +17,13 @@ server.get('/about', (req, res) => {
 })
 
 server.get('/recipes', (req, res) => {
-  return res.render('recipes');
+  return res.render('recipes', { recipes });
 })
+
+server.get('/recipe/:recipeId', (req, res) => {
+  const {recipeId} = req.params
+  const recipe = recipes[recipeId]
+  return res.render('recipe', { recipe });
+});
 
 server.listen(3000, () => console.log('Server started and listening on port 3000.'));
